0b47b1c78c8bfddfbbf0e80fe130cb343c4b8727
Artem Grodeckiy <gal20040@users.noreply.github.com>
2017-08-07 18:47:33 +0300

    скорректировал парсинг batteryPercent
    раньше был лишний %, если заряд был равен 100%.
    и сделал табулирование четырьмя пробелами.
    и вынес выше логирование заряда.

--- system-update/system-update.sh
+++ system-update/system-update.sh
@@ -15,7 +15,7 @@ wifi2='gal'
 wifi3='Innopolis'
 charging='Charging'
 full='Full'
-minChargePercent='50'
+minChargePercent='101'
 
 logPath='/home/gal20040/apps/system-update/log.txt'
 #todo как писать лог, используя переменную logPath?
@@ -24,42 +24,45 @@ echo '' >> /home/gal20040/apps/system-update/log.txt
 myssid=$(sudo iwgetid -r)
 
 if [ "$myssid" = "$wifi1" ] || [ "$myssid" = "$wifi2" ] || [ "$myssid" = "$wifi3" ]; then
-  batteryPercent=$(acpi | awk '{print $4}' | sed "s/%,//")
-  chargeStatus=$(acpi | awk '{print $3}' | sed "s/,//")
-
-  date >> /home/gal20040/apps/system-update/log.txt
-  mess='Connected to wifi: '
-  echo $mess$myssid >> /home/gal20040/apps/system-update/log.txt
-
-  if [ "$batteryPercent" -gt "$minChargePercent" ] && ( [ "$chargeStatus" = "$charging" ] || [ "$chargeStatus" = "$full" ] ); then
-    echo 'System updating start' >> /home/gal20040/apps/system-update/log.txt
-
-    sudo apt update -y
-    echo "sudo apt update -y" >> /home/gal20040/apps/system-update/log.txt
-
-    sudo apt full-upgrade -y
-    echo "sudo apt full-upgrade -y" >> /home/gal20040/apps/system-update/log.txt
-
-    sudo apt autoremove -y
-    echo "sudo apt autoremove -y" >> /home/gal20040/apps/system-update/log.txt
-
-    sudo apt autoclean -y
-    echo "sudo apt autoclean -y" >> /home/gal20040/apps/system-update/log.txt
-
-    sudo apt-get --purge autoremove -y
-    echo "sudo apt-get --purge autoremove -y" >> /home/gal20040/apps/system-update/log.txt
+    batteryPercent=$(acpi | awk '{print $4}' | sed "s/,//" | sed "s/%//")
+    chargeStatus=$(acpi | awk '{print $3}' | sed "s/,//")
 
     date >> /home/gal20040/apps/system-update/log.txt
-    echo 'System updated successfully' >> /home/gal20040/apps/system-update/log.txt
-  else
-    messChPercent='No update. Battery charge: '
+    mess='Connected to wifi: '
+    echo $mess$myssid >> /home/gal20040/apps/system-update/log.txt
+
+    messChPercent='Battery charge: '
     messChStatus='% Charge status: '
     echo $messChPercent$batteryPercent$messChStatus$chargeStatus >> /home/gal20040/apps/system-update/log.txt
-  fi
+
+    if [ "$batteryPercent" -gt "$minChargePercent" ] && ( [ "$chargeStatus" = "$charging" ] || [ "$chargeStatus" = "$full" ] ); then
+        echo 'System updating start' >> /home/gal20040/apps/system-update/log.txt
+
+        sudo apt update -y
+        echo "sudo apt update -y" >> /home/gal20040/apps/system-update/log.txt
+
+        sudo apt full-upgrade -y
+        echo "sudo apt full-upgrade -y" >> /home/gal20040/apps/system-update/log.txt
+
+        sudo apt autoremove -y
+        echo "sudo apt autoremove -y" >> /home/gal20040/apps/system-update/log.txt
+
+        sudo apt autoclean -y
+        echo "sudo apt autoclean -y" >> /home/gal20040/apps/system-update/log.txt
+
+        sudo apt-get --purge autoremove -y
+        echo "sudo apt-get --purge autoremove -y" >> /home/gal20040/apps/system-update/log.txt
+
+        date >> /home/gal20040/apps/system-update/log.txt
+        echo 'System updated successfully' >> /home/gal20040/apps/system-update/log.txt
+    else
+        mess='Insufficient charge, no update.'
+        echo $mess >> /home/gal20040/apps/system-update/log.txt
+    fi
 else
-  date >> /home/gal20040/apps/system-update/log.txt
-  mess='There is no necessary connection - no update. Connected to wifi: '
-  echo $mess$myssid >> /home/gal20040/apps/system-update/log.txt
+    date >> /home/gal20040/apps/system-update/log.txt
+    mess='There is no necessary connection - no update. Connected to wifi: '
+    echo $mess$myssid >> /home/gal20040/apps/system-update/log.txt
 fi
 
 exit 0
