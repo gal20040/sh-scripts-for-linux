ee26b91c78f0fa2d4a20b181243ac988109681e9
Artem Grodeckiy <gal20040@users.noreply.github.com>
2018-04-01 11:47:25 +0300

    установка утилит + настройка звука
    добавил команды для необходимых утилит + переделал звуковое уведомление

--- battery-check/battery-check.sh
+++ battery-check/battery-check.sh
@@ -1,4 +1,9 @@
-#выполнить в консоли:
+#проверить наличие утилит:
+#acpi - для определения заряда
+#$ sudo apt install acpi
+#для звука
+#$ sudo apt install sox
+
 #$ touch /home/gal20040/apps/battery-check/battery-check.sh
 #$ sudo chmod +x /home/gal20040/apps/battery-check/battery-check.sh
 
@@ -16,21 +21,20 @@ discharging='Discharging'
 if [ "$chargeStatus" = "$discharging" ]; then
 
   batteryPercent=$(acpi | awk '{print $4}' | sed "s/%,//")
-  if [ "$batteryPercent" -le "10" ]; then
+  if [ "$batteryPercent" -le "15" ]; then
+    #todo - звук не всегда слышен, если запуск идёт через crontab; если же запускать скрипт вручную, то всё ок.
+    play -n synth 0.1 sin 2500
+    sleep 0.1
+    if [ "$batteryPercent" -le "10" ]; then
+      play -n synth 0.1 sin 2500
+      sleep 0.1
+      play -n synth 0.1 sin 2500
+      sleep 0.1
+      play -n synth 0.1 sin 2500
+      play -n synth 1 sin 4100
+    fi
     DISPLAY=:0 gdialog --msgbox "Battery discharching" 25 20 > /dev/null
   fi
-  
-#todo - звук не слышен, если запуск идёт через crontab; если же запускать скрипт вручную, то всё ок.
-#если разобраться со звуком, то можно настроить разную тональность и продолжительность (т.е. надоедливость) для разного уровня заряда.
-#  if [ "$batteryPercent" -gt "5" ] && [ "$batteryPercent" -le "10" ]; then
-#    play -n synth 0.1 sin 2500
-#    sleep 0.1
-#    play -n synth 0.1 sin 2500
-#    sleep 0.1
-#    play -n synth 0.1 sin 2500 #это для просто низкого заряда
-#    play -n synth 1 sin 4100 #это для совсем тревоги - громче и продолжительнее
-#    DISPLAY=:0 gdialog --msgbox "Battery discharching" 25 20 > /dev/null
-#  fi
 fi
 
 exit 0
